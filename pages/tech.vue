d40aed71d39998adecba909a408e5fd0e681981a
Geoff St. Pierre <serundeputy@gmail.com>
2017-09-01 10:43:13 -0400

    thinktandem/horoscope#86: Example two col layout.

---
+++ pages/tech.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="all-the-content">
+    <div class="container layout--lando-two-col">
+      <div class="row layout--lando-two-col-row">
+        <div class="col-3">
+          <h3>Technologies</h3>
+          <a href="#backdrop" class="layout--lando-two-col-left-nav">Backdrop</a>
+          <a href="#drupal" class="layout--lando-two-col-left-nav">Drupal</a>
+          <a href="#laravel" class="layout--lando-two-col-left-nav">Laravel</a>
+        </div>
+        <div class="col-9">
+          <h1>All the Tech</h1>
+          <p>
+            We do tech nice; all the techs; even more tecs.
+          </p>
+        </div>
+      </div>
+    </div>
+    <foot/>
+  </section>
+</template>
+
+<script src="../assets/js/index.js"></script>
+<style src="../assets/css/styles.css"></style>
+<style src="../assets/css/tech.css"></style>
